1afde2bf51b2325ba8c2aa36b302614487ba1876
7meed <abdulhameed.khaled@everestminds.com>
2026-07-25 03:49:52 +0300

    use strapi own role premission guard

--- src/plugins/inventory-dashboard/server/src/bootstrap.ts
+++ src/plugins/inventory-dashboard/server/src/bootstrap.ts
@@ -1,7 +1,14 @@
 import type { Core } from "@strapi/strapi";
 
-const bootstrap = ({ strapi }: { strapi: Core.Strapi }) => {
-  // bootstrap phase
+const bootstrap = async ({ strapi }: { strapi: Core.Strapi }) => {
+  await strapi.service("admin::permission").actionProvider.registerMany([
+    {
+      section: "plugins",
+      displayName: "Access the Inventory Dashboard",
+      uid: "access",
+      pluginName: "inventory-dashboard",
+    },
+  ]);
 };
 
 export default bootstrap;
